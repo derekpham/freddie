1684fd653228c77280dad89e2a4c5f96b2711f0f
Derek Pham <anhdpham97@gmail.com>
2018-11-19 00:23:41 -0500

    project init

---
+++ src/main/java/com/derek/freddie/FreddieApplication.java
@@ -0,0 +1,12 @@
+package com.derek.freddie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreddieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreddieApplication.class, args);
+    }
+}
---
+++ src/test/java/com/derek/freddie/FreddieApplicationTests.java
@@ -0,0 +1,16 @@
+package com.derek.freddie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreddieApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
